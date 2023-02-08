12bb2d63bc8a25f9a2ef05de35c870b8891dd40b
Arnav Guneta <arnavguneta@gmail.com>
2023-02-07 21:55:39 -0500

    update api and rebuild

--- dist/routes/spotify.js
+++ dist/routes/spotify.js
@@ -3,18 +3,28 @@ import fetch from 'node-fetch';
 import clientAuth from '../middleware/clientCreds.js';
 import authMiddleware from '../middleware/auth.js';
 const router = express.Router();
+const TRACK_LIMIT = 3000;
+const PLAYLIST_LIMIT = 1000;
 router.get('/me/playlists', authMiddleware, async (req, res) => {
-    const response = await fetch(`${process.env.SPOTIFY_API_URL}/me/playlists?limit=50`, {
+    const offset = parseInt(req.query.offset) || 0;
+    if (offset >= PLAYLIST_LIMIT)
+        return res.status(200).json({ items: [], offset });
+    const response = await fetch(`${process.env.SPOTIFY_API_URL}/me/playlists?limit=50&offset=${offset}`, {
         headers: {
             'Authorization': `Bearer ${req.accessToken}`
         }
     });
     const data = await response.json();
-    res.status(response.status).json(data.items);
+    if (response.ok)
+        return res.status(response.status)
+            .json({ ...data, offset: offset + data.items.length });
+    else
+        return res.status(response.status).json();
 });
 router.get('/playlists/:id/tracks', clientAuth, async (req, res) => {
-    console.log(req.query.offset);
     const offset = parseInt(req.query.offset) || 0;
+    if (offset >= TRACK_LIMIT)
+        return res.status(200).json({ items: [], offset });
     const response = await fetch(
     // eslint-disable-next-line max-len
     `${process.env.SPOTIFY_API_URL}/playlists/${req.params.id}/tracks?fields=items(added_at%2Ctrack(album(name%2Cexternal_urls%2Cid%2Cimages)%2Cartists(external_urls%2Cname%2Cid)%2Cexplicit%2Cduration_ms%2Cexternal_urls%2Cid%2Cname))&limit=50&offset=${offset}`, {
@@ -23,9 +33,11 @@ router.get('/playlists/:id/tracks', clientAuth, async (req, res) => {
         }
     });
     const data = await response.json();
-    console.log(offset, response.status);
-    res.status(response.status)
-        .json({ items: data.items, offset: offset + data.items.length });
+    if (response.ok)
+        return res.status(response.status)
+            .json({ items: data.items, offset: offset + data.items.length });
+    else
+        return res.status(response.status).json();
 });
 router.get('/playlists/:id/', clientAuth, async (req, res) => {
     const response = await fetch(
--- src/routes/spotify.ts
+++ src/routes/spotify.ts
@@ -6,23 +6,32 @@ import authMiddleware from '../middleware/auth.js';
 
 const router: Router = express.Router();
 
+const TRACK_LIMIT = 3000;
+const PLAYLIST_LIMIT = 1000;
+
 router.get('/me/playlists', authMiddleware,
   async (req: Request, res: Response) => {
+    const offset = parseInt(<string>req.query.offset) || 0;
+    if (offset >= PLAYLIST_LIMIT)
+      return res.status(200).json({ items: [], offset });
     const response = await fetch(
-      `${process.env.SPOTIFY_API_URL}/me/playlists?limit=50`, {
+      `${process.env.SPOTIFY_API_URL}/me/playlists?limit=50&offset=${offset}`, {
       headers: {
         'Authorization': `Bearer ${req.accessToken}`
       }
     });
     const data = <PlaylistResponse>await response.json();
-    res.status(response.status).json(data.items);
+    if (response.ok) return res.status(response.status)
+      .json({ ...data, offset: offset + data.items.length });
+    else return res.status(response.status).json();
   }
 );
 
 router.get('/playlists/:id/tracks', clientAuth,
   async (req: Request, res: Response) => {
-    console.log(req.query.offset);
     const offset = parseInt(<string>req.query.offset) || 0;
+    if (offset >= TRACK_LIMIT)
+      return res.status(200).json({ items: [], offset });
     const response = await fetch(
       // eslint-disable-next-line max-len
       `${process.env.SPOTIFY_API_URL}/playlists/${req.params.id}/tracks?fields=items(added_at%2Ctrack(album(name%2Cexternal_urls%2Cid%2Cimages)%2Cartists(external_urls%2Cname%2Cid)%2Cexplicit%2Cduration_ms%2Cexternal_urls%2Cid%2Cname))&limit=50&offset=${offset}`, {
@@ -31,9 +40,9 @@ router.get('/playlists/:id/tracks', clientAuth,
       }
     });
     const data = <TrackResponse>await response.json();
-    console.log(offset, response.status);
-    res.status(response.status)
-    .json({items: data.items, offset: offset + data.items.length});
+    if (response.ok) return res.status(response.status)
+      .json({ items: data.items, offset: offset + data.items.length });
+    else return res.status(response.status).json();
   }
 );
 
